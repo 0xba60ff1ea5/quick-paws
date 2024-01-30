6edb63e5a8513faa0894028fdc46dbf9b641ce7c
0xBA60FF1EA5 <0xba60ff1ea5@gmail.com>
2024-01-29 22:00:03 -0600

    Second commit

---
+++ bake_cookies.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import time
+from selenium import webdriver
+
+TOP = os.path.realpath(__file__).rsplit('/', 1)[0] + '/'
+
+def main():
+    """
+    Script for first-time setup of cookies
+    TODO: Figure out how to use the User's browser of choice, somewhere in settings.json
+    TODO: Figure out which site to get cookies from
+    """
+    url = "https://www.furaffinity.net/login/"
+
+    # Prompt user to log in
+    session = webdriver.Firefox()
+    session.get(url)
+    # TODO: Find a better solution than this...
+    print("Waiting 60 seconds for user to log in...")
+    time.sleep(60)
+
+    # Store cookies in .json file
+    with open(TOP + 'src/config/cookies.json', 'w') as filehandler:
+        json.dump(session.get_cookies(), filehandler)
+
+if __name__ == "__main__":
+    retval = main()
+    raise SystemExit(retval)
